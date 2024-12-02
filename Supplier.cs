ce416eb2a282068dd3ddf60e1a24b4114cb94292
VadimSavintsev <161233776+VadimSavintsev@users.noreply.github.com>
2024-12-02 08:31:28 +0300

    lab 1 task 6 JSON

--- Supplier.cs
+++ Supplier.cs
@@ -1,8 +1,9 @@
 ﻿
 using System;
 using System.Text.RegularExpressions;
-
-	public class Supplier
+using Newtonsoft.Json;
+ 
+public class Supplier
 {
     private int? id;
     private string name;
@@ -11,7 +12,8 @@ using System.Text.RegularExpressions;
     private string email;
     private string inn;
     private string ogrn;
-
+ 
+    [JsonConstructor]
     public Supplier(string name, string address, string phoneNumber, string email, string inn, string ogrn)
     {
         SetName(name);
@@ -25,47 +27,47 @@ using System.Text.RegularExpressions;
     {
         return !string.IsNullOrEmpty(name) && Regex.IsMatch(name, "^[А-Яа-я\\s-]+$");
     }
-
+ 
     public static bool ValidateAddress(string address)
     {
         return !string.IsNullOrEmpty(address) && Regex.IsMatch(address, "^[А-Яа-я\\s-]+,\\s*[А-Яа-я\\s,.\\-]+,\\s*\\d+$");
     }
-
+ 
     public static bool ValidatePhoneNumber(string phoneNumber)
     {
         return !string.IsNullOrEmpty(phoneNumber) && Regex.IsMatch(phoneNumber, @"\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}");
     }
-
+ 
     public static bool ValidateEmail(string email)
     {
         return !string.IsNullOrEmpty(email) && Regex.IsMatch(email, @"(.+)@(.+)\.(.+)");
     }
-
+ 
     public static bool ValidateInn(string inn)
     {
         return !string.IsNullOrEmpty(inn) && Regex.IsMatch(inn, @"\d{10}");
     }
-
+ 
     public static bool ValidateOgrn(string ogrn)
     {
         return !string.IsNullOrEmpty(ogrn) && Regex.IsMatch(ogrn, @"\d{13}");
     }
-
+ 
     public int? GetId()
     {
         return id;
     }
-
+ 
     public void SetId(int? id)
     {
         this.id = id;
     }
-
+ 
     public string GetName()
     {
         return name;
     }
-
+ 
     public void SetName(string name)
     {
         if (ValidateName(name))
@@ -77,12 +79,12 @@ using System.Text.RegularExpressions;
             throw new ArgumentException("Название должно содержать только русские символы");
         }
     }
-
+ 
     public string GetAddress()
     {
         return address;
     }
-
+ 
     public void SetAddress(string address)
     {
         if (ValidateAddress(address))
@@ -94,12 +96,12 @@ using System.Text.RegularExpressions;
             throw new ArgumentException("Адрес должен содержать через запятую город, улицу и дом с номером");
         }
     }
-
+ 
     public string GetPhoneNumber()
     {
         return phoneNumber;
     }
-
+ 
     public void SetPhoneNumber(string phoneNumber)
     {
         if (ValidatePhoneNumber(phoneNumber))
@@ -111,12 +113,12 @@ using System.Text.RegularExpressions;
             throw new ArgumentException("Телефон должен содержать цифры и быть записан в формате: +7 (***) ***-**-**");
         }
     }
-
+ 
     public string GetEmail()
     {
         return email;
     }
-
+ 
     public void SetEmail(string email)
     {
         if (ValidateEmail(email))
@@ -128,12 +130,12 @@ using System.Text.RegularExpressions;
             throw new ArgumentException("Неверно введён адрес электронной почты");
         }
     }
-
+ 
     public string GetInn()
     {
         return inn;
     }
-
+ 
     public void SetInn(string inn)
     {
         if (ValidateInn(inn))
@@ -145,12 +147,12 @@ using System.Text.RegularExpressions;
             throw new ArgumentException("ИНН должен содержать 10 цифр");
         }
     }
-
+ 
     public string GetOgrn()
     {
         return ogrn;
     }
-
+ 
     public void SetOgrn(string ogrn)
     {
         if (ValidateOgrn(ogrn))
@@ -163,23 +165,33 @@ using System.Text.RegularExpressions;
         }
     }
 }
-	class Program
-	{
-		public static void Main(string[] args)
-		{
-			Supplier supplier = new Supplier(
-            name: "ИП Иванов-Петров",
-            address: "Санкт-Петербург, Невский проспект, 45",
-            phoneNumber: "+7 (812) 987-65-43",
-            email: "info@kopytairoga.ru",
-            inn: "0987654321",
-            ogrn: "3210987654321"
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Supplier supplier = new Supplier(
+        name: "ИП Иванов-Петров",
+        address: "Санкт-Петербург, Невский проспект, 45",
+        phoneNumber: "+7 (812) 987-65-43",
+        email: "info@kopytairoga.ru",
+        inn: "0987654321",
+        ogrn: "3210987654321"
         );
-        	Console.WriteLine(supplier.GetId());
-        	Console.WriteLine(supplier.GetName());
-        	Console.WriteLine(supplier.GetAddress());
-        	supplier.SetEmail("newemail@example.com");
-        	Console.WriteLine(supplier.GetEmail());
-        	Console.ReadKey(true);
-		}
-	}
+        string json = @"{
+            ""name"": ""ИП Петров"",
+            ""address"": ""Москва, Невский проспект, 45"",
+            ""phoneNumber"": ""+7 (812) 987-65-43"",
+            ""email"": ""info@kopytairoga.ru"",
+            ""inn"": ""0987654321"",
+            ""ogrn"": ""3210987654321""
+        }";
+        Supplier sup2 = JsonConvert.DeserializeObject<Supplier>(json);
+        Console.WriteLine(supplier.GetId());
+        Console.WriteLine(supplier.GetName());
+        Console.WriteLine(supplier.GetAddress());
+        supplier.SetEmail("newemail@example.com");
+        Console.WriteLine(supplier.GetEmail());
+        Console.WriteLine(sup2.GetName());
+        Console.ReadKey(true);
+    }
+}
