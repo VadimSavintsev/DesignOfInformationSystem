ae5acf9d60e6172443a7cc12dd8f2d5d58f83fee
VadimSavintsev <161233776+VadimSavintsev@users.noreply.github.com>
2024-12-15 16:51:08 +0300

    lab 2 task 3 inheritance
    Изменён класс SupplierRepYaml на SupplierYamlStrategy

---
+++ SupplierYamlStrategy.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+using Project;
+
+namespace Program
+{
+     class SupplierYamlStrategy : SupplierStrategy
+    {
+       private readonly string yamlfilePath;
+
+       public SupplierYamlStrategy(string yamlfilePath)
+        {
+            this.yamlfilePath = yamlfilePath;
+        }
+
+        public SupplierRepYaml(string filePath)
+        {
+            _filePath = filePath;
+            if (!File.Exists(_filePath))
+            {
+                File.WriteAllText(_filePath, "[]"); // Создаем пустой YAML-файл, если его нет
+            }
+        }
+
+        // Чтение всех значений из файла
+        public List<Supplier> ReadAllValues()
+        {    
+            try
+            {
+                var yaml = File.ReadAllText(_filePath);
+                var deserializer = new DeserializerBuilder()
+                    .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                    .Build();
+                return deserializer.Deserialize<List<Supplier>>(yaml);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading YAML file: {ex.Message}");
+                return new List<Supplier>(); // Возвращаем пустой список в случае ошибки
+            }
+        }
+
+        // Запись всех значений в файл
+        public void WriteAllValues(List<Supplier> data)
+        {
+            try
+            {
+                var serializer = new SerializerBuilder()
+                    .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                    .Build();
+                var yaml = serializer.Serialize(data);
+                File.WriteAllText(_filePath, yaml);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error writing YAML file: {ex.Message}");
+            }
+        }
+    }
+}
