d1ea71cce3e51aaba022806ef07e2a0f3ce4c31d
VadimSavintsev <161233776+VadimSavintsev@users.noreply.github.com>
2024-12-15 16:52:53 +0300

    lab 2 task 3 inheritance
    Изменение класса SupplierRepJson на SupplierJsonStrategy

---
+++ SupplierJsonStrategy.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Project;
+
+namespace Program
+{
+    class SupplierJsonStrategy : SupplierStrategy
+    {
+        private readonly string jsonfilePath;
+
+        public SupplierJsonStrategy(string jsonfilePath)
+        {
+            this.jsonfilePath = jsonfilePath;
+        }
+
+        public List<Supplier> ReadAllValues()
+        {
+            try
+            {
+                var json = File.ReadAllText(jsonfilePath);
+                return JsonConvert.DeserializeObject<List<Supplier>>(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading JSON file: {ex.Message}");
+                return new List<Supplier>(); // Возвращаем пустой список в случае ошибки
+            }
+        }
+
+        public void WriteAllValues(List<Supplier> data)
+        {
+            try
+            {
+                var json = JsonConvert.SerializeObject(data, Formatting.Indented);
+                File.WriteAllText(jsonfilePath, json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error writing JSON file: {ex.Message}");
+            }
+        }
+    }
+}
