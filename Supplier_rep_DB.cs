eb3ac31bbcf2a84ae71a2c5a27a373e12e9a0377
VadimSavintsev <161233776+VadimSavintsev@users.noreply.github.com>
2025-02-08 00:28:27 +0300

    lab 2 task 4.b Supplier_rep_DB
    Реализован метод вывода списка k элементов по счету n объектов

--- Supplier_rep_DB.cs
+++ Supplier_rep_DB.cs
@@ -9,20 +9,17 @@ public class Supplier_rep_DB
     {
         connectionString = connectionString;
     }
-    public Supplier GetSupplierById(int supplierId)
+        public Supplier GetSupplierById(int supplierId)
     {
-        var query = "SELECT * FROM Suppliers WHERE SupplierID = @SupplierID;";
-
+        var query = "SELECT * FROM Suppliers WHERE Id = @Id;";
         using (var connection = new NpgsqlConnection(connectionString))
         {
             try
             {
                 connection.Open();
-
                 using (var command = new NpgsqlCommand(query, connection))
                 {
-                    command.Parameters.AddWithValue("@SupplierID", supplierId);
-
+                    command.Parameters.AddWithValue("@Id", supplierId);
                     using (var reader = command.ExecuteReader())
                     {
                         if (reader.Read())
@@ -36,7 +33,6 @@ public class Supplier_rep_DB
                                 inn: reader.GetString(reader.GetOrdinal("Inn")),
                                 ogrn: reader.GetString(reader.GetOrdinal("Ogrn"))
                             );
-
                             return supplier;
                         }
                         else
@@ -50,6 +46,45 @@ public class Supplier_rep_DB
             {
                 Console.WriteLine($"Ошибка при выполнении запроса: {ex.Message}");
                 return null;
+            }
+        }
+    }
+
+    public List<SupplierShort> GetKNSuppliers(int k, int n)
+    {
+        var query = "SELECT * FROM Suppliers ORDER BY Id LIMIT @Limit OFFSET @Offset;";
+        using (var connection = new NpgsqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                using (var command = new NpgsqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Limit", k);
+                    command.Parameters.AddWithValue("@Offset", n);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        var suppliers = new List<SupplierShort>();
+                        while (reader.Read())
+                        {
+                            var supplier = new SupplierShort(
+                                id: reader.GetInt32(reader.GetOrdinal("Id")),
+                                name: reader.GetString(reader.GetOrdinal("Name")),
+                                phoneNumber: reader.GetString(reader.GetOrdinal("PhoneNumber")),
+                                inn: reader.GetString(reader.GetOrdinal("Inn")),
+                                ogrn: reader.GetString(reader.GetOrdinal("Ogrn"))
+                            );
+
+                            suppliers.Add(supplier);
+                        }
+                        return suppliers;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при выполнении запроса: {ex.Message}");
+                return new List<SupplierShort>();
             }
         }
     }
